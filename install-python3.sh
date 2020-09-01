b3058b41c68c372d9e913acb54b3b2b7751161d1
GOTO Satoru <satoru.goto@appdynamics.com>
2020-09-01 10:11:02 +0900

    update start.sh

--- install-python3.sh
+++ install-python3.sh
@@ -6,4 +6,4 @@ virtualenv -p python3 python3
 source python3/bin/activate
 python3/bin/pip3 install -r requirements.txt
 python3/bin/pip3 install appdynamics
-
+python3/bin/pip3 list
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
+#!/bin/sh -x
 
-# This is a script to start the Python demo app
-
-nohup python3/bin/pyagent run -c appdynamics.cfg - pyton3/bin/gunicorn -w 2 -b 0.0.0.0:9000 demo.app:app &
+ENV=python3
+$ENV/bin/pyagent run -c appdynamics.cfg - $ENV/bin/gunicorn -w 2 -b 0.0.0.0:9000 demo.app:app 
